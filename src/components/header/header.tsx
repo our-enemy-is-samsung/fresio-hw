e5d8aeb82d7ceb8b264a61fac6b61c5f2c166e19
officeshinyujun <officeshinyujun@gmail.com>
2024-11-20 01:29:55 +0900

    error

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,20 @@
 import styles from "./header.module.scss"
 
 export default function Header() {
+
+    const DateFunction = () => {
+        const date = new Date();
+        return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
+    }
+
     return (
         <div className={styles.container}>
+            <div>
+                {DateFunction()}
+            </div>
+            <div>
 
+            </div>
         </div>
     )
 }
---
+++ src/pages/main/mainpage.tsx
@@ -0,0 +1,9 @@
+import Header from "../../components/header/header.tsx";
+
+export default function Mainpage() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
