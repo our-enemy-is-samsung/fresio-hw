1b1f9f1f6d671e46683d5bb1baa57d3fb6369ae6
officeshinyujun <officeshinyujun@gmail.com>
2024-11-19 13:24:06 +0900

    feat/컬러 시스템 추가

---
+++ src/types/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+    "surface" : '#fff',
+    "surfaceDim" : '#f5f5f5',
+    "brand" : '#f3902f',
+    "bradnDim" : '#faa75f',
+    "brandDark" : '#d67b24',
+    "brandContainer" : '#fff3e8',
+    "error" : '#f04438',
+    "errorBackground" : '#ffe9e3',
+    "success" : '#12b76a',
+    "successBackground" : '#e6f9ee',
+    "content" : '#141414',
+    "contentDim" : '#707085',
+    "contentSecondary" : '#b4b4c8',
+    "containerDark" : '#ededf5',
+    "containerDarker" : '#e3e3f0',
+    "containerDarkest" : '#d5d5e5'
+}
